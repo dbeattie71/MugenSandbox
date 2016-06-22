744511c777d874c7bd8bae38349b648d4134f9fc
Derek Beattie <derek.beattie@gmail.com>
2016-06-22 16:54:44 -0500

    mc

--- MugenSandbox.Core/ViewModels/MainViewModel.cs
+++ MugenSandbox.Core/ViewModels/MainViewModel.cs
@@ -1,8 +1,47 @@
-﻿using MugenMvvmToolkit.ViewModels;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MugenMvvmToolkit;
+using MugenMvvmToolkit.Interfaces.Navigation;
+using MugenMvvmToolkit.Interfaces.ViewModels;
+using MugenMvvmToolkit.Models;
+using MugenMvvmToolkit.ViewModels;
 
 namespace MugenSandbox.Core.ViewModels
 {
-    public class MainViewModel : CloseableViewModel
+    public class MainViewModel : CloseableViewModel, INavigableViewModel
     {
+        private bool _isFirstNavigation;
+
+        public MainViewModel()
+        {
+            ChangeSomeStringCommand = new RelayCommand(ButtonTest);
+        }
+
+        public ICommand ChangeSomeStringCommand { get; private set; }
+
+        public string SomeString { get; set; }
+
+        public void OnNavigatedTo(INavigationContext context)
+        {
+            if (_isFirstNavigation)
+                return;
+
+            SomeString = DateTime.Now.ToString();
+        }
+
+        public Task<bool> OnNavigatingFrom(INavigationContext context)
+        {
+            return Empty.TrueTask;
+        }
+
+        public void OnNavigatedFrom(INavigationContext context)
+        {
+        }
+
+        private void ButtonTest()
+        {
+            SomeString = DateTime.Now.ToString();
+        }
     }
 }
--- MugenSandbox.iOS/Views/MainView.cs
+++ MugenSandbox.iOS/Views/MainView.cs
@@ -1,26 +1,48 @@
 using System;
-using Foundation;
 using MugenMvvmToolkit.Attributes;
+using MugenMvvmToolkit.Binding;
+using MugenMvvmToolkit.Binding.Builders;
 using MugenMvvmToolkit.iOS.Views;
 using MugenSandbox.Core.ViewModels;
-using UIKit;
 
 namespace MugenSandbox.iOS.Views
 {
-    [ViewModel(typeof(MainViewModel))]
+    [ViewModel(typeof (MainViewModel))]
     public partial class MainView : MvvmViewController
     {
+        private string _someString;
+
         public MainView() : base("MainView", null)
         {
         }
 
-       
+        public string SomeString
+        {
+            get
+            {
+                return _someString;
+            }
+            set
+            {
+                _someString = value;
+
+                if (value !=null)
+                    SomeStringLb.Text = value;
+            }
+        }
+
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
-            // Perform any additional setup after loading the view, typically from a nib.
+            using (var set = new BindingSet<MainViewModel>())
+            {
+                set.Bind(ChangeSomeStringBtn).To(() => (vm, ctx) => vm.ChangeSomeStringCommand);
+                set.Bind(this, nameof(SomeString)).To(() => (vm, ctx) => vm.SomeString).TwoWay();
+            }
         }
+
+        public event EventHandler SomeStringChanged;
     }
 }
